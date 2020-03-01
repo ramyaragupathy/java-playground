72e9c2aeb06f8388089b9cbc57c82b7489cdcdfd
ramyaragupathy <r.ragupathy85@gmail.com>
2020-03-01 19:04:45 +0530

    Add a method on class

--- Employee.java
+++ Employee.java
@@ -3,9 +3,17 @@ class Employee {
     String name;
     float sal;
 
+    void getDetails(){
+         System.out.println("From getDetails(): "  
+            + empno 
+            + "|" 
+            + name  
+            + "|"  
+            + sal);
+    }
     public static void main (String[] args){
         Employee emp1 = new Employee();
-        System.out.println(emp1.empno + "|" + emp1.name + "|" + emp1.sal);
+        emp1.getDetails();
     }
 }
 
