b4cdde8cc39247b79138bd00afe2324e2d2ce46c
ramyaragupathy <r.ragupathy85@gmail.com>
2020-03-01 18:55:43 +0530

    Object instantiation example

---
+++ Employee.java
@@ -0,0 +1,10 @@
+class Employee {
+    int empno;
+    String name;
+    float sal;
+
+    public static void main (String[] args){
+        Employee emp1 = new Employee();
+        System.out.println(emp1.empno + "|" + emp1.name + "|" + emp1.sal);
+    }
+}
